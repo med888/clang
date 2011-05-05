a06ef5cdabeb7c70cbf24605bf52447cf468b4ee
Glenn Ganz <glenn.ganz@uptime.ch>
2011-05-05 17:29:52 +0200

    -fcocotron-runtime flag added

---
+++ lib/CodeGen/CGObjCCocotron.cpp
@@ -0,0 +1,17 @@
+//===--- CGObjCCocotron.cpp - Interface to Cocotron Objective-C Runtime ---===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This provides Objective-C code generation targeting the Cocotron runtime.
+//
+//===----------------------------------------------------------------------===//
+
+CGObjCRuntime *
+clang::CodeGen::CreateCocotronObjCRuntime(CodeGenModule &CGM) {
+    return null;
+}
